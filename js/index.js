12ba9a56f4c5aeb0dc61a2d746149ed4b6c8e8ea
shadownetz <okwuzuchiagozie@gmail.com>
2021-01-06 13:15:25 +0100

    update styling

--- js/index.js
+++ js/index.js
@@ -26,7 +26,7 @@
         }
         let currentPage = 1;
         let selectedOption = '';
-        let totalNumberOfAddressField = 4;
+        let totalNumberOfAddressField = 3;
         let addressValues = [];
         const addressPayload = {
             addresses: [],
@@ -40,8 +40,8 @@
 
         addAddressField.on('click', event=>{
             event.preventDefault();
+            appendAddressField(totalNumberOfAddressField);
             totalNumberOfAddressField++;
-            appendAddressField(totalNumberOfAddressField)
         });
 
         toQuoteBtn.on('click', event=>{
@@ -50,6 +50,7 @@
         })
 
         $('.bck-scnd-pg').on('click', ()=>gotoPage(1))
+        $('.bck-scnd-pg-2').on('click', ()=>gotoPage(1))
         $('#newQuote').on('click', ()=>gotoPage(1))
 
         let pricingList = [];
@@ -86,15 +87,15 @@
         function togglePageElements(page=1){
             if(page === 1){
                 $('.hero-side').fadeIn();
-                $('.extra').fadeOut()
-                secondScreen.fadeOut('fast');
-                nextStepTxt.fadeOut('fast');
-                decisionTxt2.fadeOut('fast')
-                addressAlert.fadeOut('fast');
-                addressActions.fadeOut('fast');
-                addAddressField.fadeOut('fast');
-                finalStepTxt.fadeOut('fast')
-                decisionTxt3.fadeOut('fast')
+                $('.extra').hide()
+                secondScreen.hide();
+                nextStepTxt.hide();
+                decisionTxt2.hide()
+                addressAlert.hide();
+                addressActions.hide();
+                addAddressField.hide();
+                finalStepTxt.hide()
+                decisionTxt3.hide()
                 addressBlockElement.empty();
                 servicesBlock.empty();
 
@@ -106,18 +107,18 @@
             }else if(page === 2){
                 if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
                     || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) {
-                   $('.hero-side').fadeOut();
+                   $('.hero-side').hide();
                    $('.extra').fadeIn()
                 }
                 selectedOptionDisplayElem.text(`${selectedOption} installations require ${defaultNumberOfAddresses[selectedOption]===1?'only': ''} ${defaultNumberOfAddresses[selectedOption]} ${defaultNumberOfAddresses[selectedOption]>=3?'or more': ''} address${defaultNumberOfAddresses[selectedOption]>1?'es': ''}`)
-                firstScreen.fadeOut('fast');
-                currStepTxt.fadeOut('fast');
-                decisionTxt1.fadeOut('fast');
-                optionElem.fadeOut('fast');
+                firstScreen.hide();
+                currStepTxt.hide();
+                decisionTxt1.hide();
+                optionElem.hide();
                 defaultNumberOfAddresses[selectedOption]>=3? addAddressField.fadeIn('slow'):addAddressField.fadeOut('slow');
                 locationPreview.text('');
                 addressValues = [];
-
+                totalNumberOfAddressField = 3;
 
                 secondScreen.fadeIn('slow');
                 nextStepTxt.fadeIn('slow');
@@ -125,16 +126,16 @@
                 addressAlert.fadeIn('slow');
                 addressActions.fadeIn('slow')
             }else if(page === 3){
-                nextStepTxt.fadeOut('fast');
-                decisionTxt2.fadeOut('fast');
-                addressAlert.fadeOut('fast');
-                addressActions.fadeOut('fast');
+                nextStepTxt.hide();
+                decisionTxt2.hide();
+                addressAlert.hide();
+                addressActions.hide();
                 addressBlockElement.empty();
 
                 finalStepTxt.fadeIn('slow')
                 decisionTxt3.fadeIn('slow').css('display', 'block');
                 if(addressPayload.addresses.length > 0){
-                    const tmp_list =  addressValues.map((address, index)=>`#${index+1}. ${address}`);
+                    const tmp_list =  addressPayload.addresses.map((address, index)=>`#${index+1}. ${address.address}`);
                     locationPreview.html(tmp_list.join('<br>'))
                 }else{
                     locationPreview.text('')
@@ -169,7 +170,7 @@
             let autocomplete = new google.maps.places.Autocomplete(input);
             google.maps.event.addListener(autocomplete, 'place_changed', function () {
                 let place = autocomplete.getPlace();
-                $('#address_'+fieldCount).val(place.formatted_address)
+                $('#address_'+fieldCount).val(place.formatted_address);
                 addressPayload.addresses[fieldCount].address = place.formatted_address;
                 addressPayload.addresses[fieldCount].latitude = place.geometry.location.lat();
                 addressPayload.addresses[fieldCount].longitude = place.geometry.location.lng();
@@ -222,7 +223,7 @@
                                             </h4>
                                             <div class="row justify-content-center align-content-center">
                                                 <a id="pricing_${i}" href="javascript:void(0)" class="text-muted pt-2 font-weight-bolder" data-toggle="modal" data-target="#quoteRequest">
-                                                    <img src="./images/icons/envelop.svg" alt="line">&nbsp;GET QUOTE
+                                                    <img src="./images/icons/envelop.svg" alt="line">&nbsp; ${pricing_list[i].min_cost==='ICB'?'REQUEST QUOTE': 'GET QUOTE'}
                                                 </a>
                                             </div>
                                         </div>
